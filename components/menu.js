1d4ee7649245205268c49554f45bebb34ea4cfce
slay trunks <108131182+SlayTrunks@users.noreply.github.com>
2024-04-17 10:36:44 +0545

    Update menu.js

--- components/menu.js
+++ components/menu.js
@@ -5,10 +5,10 @@ const menu = () => {
     <div className="menu">
   <div className="label">Follow Me</div>
   <div className="spacer"></div>
-  <div className="item"><a href='https://twitter.com/slaytrunks' target='_blank'>Twitter</a></div>
-  <div className="item"><a href='https://instagram.com/diwas.js' target='_blank'>Instagram</a></div>
+
+  <div className="item"><a href='https://instagram.com/' target='_blank'>Instagram</a></div>
   <div className="item"><a href='https://github.com/slaytrunks' target='_blank'>Github</a></div>
-  <div className="item"><a href='https://facebook.com/diwas69' target='_blank'>Facebook</a></div>
+  <div className="item"><a href='https://facebook.com/' target='_blank'>Facebook</a></div>
 
 </div>
   )
